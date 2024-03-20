7a06eb290a2f3bace76ea858b09e3effbf80979a
JinXx ! <163841101+JinX-7@users.noreply.github.com>
2024-03-20 01:35:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ res.sendFile(__path + '/main.html')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
-    console.log(`YoutTube: @DGXeon\nTelegram: xeonbotinc\nGitHub: @DGXeon\nInstsgram: unicorn_xeon13\n\nServer running on http://localhost:` + PORT)
+    console.log(`usergit @JinX-7` + PORT)
 })
 
 module.exports = app
